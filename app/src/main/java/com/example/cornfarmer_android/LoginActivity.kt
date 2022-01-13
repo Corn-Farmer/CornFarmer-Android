cd9a077ddc23064bcdc18aae750ae271450f1a53
lee989898 <wnsrud1352@gmail.com>
2022-01-13 14:23:29 +0900

    feat: joinott 액티비티 추가
    join ott 액티비티 추가 및 login id 추가

--- app/src/main/java/com/example/cornfarmer_android/LoginActivity.kt
+++ app/src/main/java/com/example/cornfarmer_android/LoginActivity.kt
@@ -1,13 +1,27 @@
 package com.example.cornfarmer_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cornfarmer_android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginKakaoLoginBt.setOnClickListener {
+            val intent = Intent(this, JoinProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
