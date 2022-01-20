daa63e5bd33023501225aa41664156d99a50d82f
lee989898 <wnsrud1352@gmail.com>
2022-01-20 20:28:24 +0900

    feat: join profile
    join profile 카메라 선택 다이얼 로그 구현

--- app/src/main/java/com/example/cornfarmer_android/JoinProfileActivity.kt
+++ app/src/main/java/com/example/cornfarmer_android/JoinProfileActivity.kt
@@ -1,17 +1,67 @@
 package com.example.cornfarmer_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import com.example.cornfarmer_android.databinding.ActivityJoinProfileBinding
 
 class JoinProfileActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityJoinProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join_profile)
+        binding = ActivityJoinProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.profileImagePlusIv.setOnClickListener{
+            showDialog()
+        }
 
 
 
     }
+
+    private fun showDialog(){
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.image_select_custom_dialog, null)
+        val mBuilder = AlertDialog.Builder(this, R.style.CustomAlertDialog)
+            .setView(mDialogView)
+
+        val alertDialog = mBuilder.show()
+        alertDialog.window?.setGravity(Gravity.BOTTOM)
+
+
+
+        alertDialog.findViewById<Button>(R.id.select_camera_bt)?.setOnClickListener {
+
+        }
+
+        alertDialog.findViewById<Button>(R.id.select_album_bt)?.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 100)
+        }
+//
+//        alertDialog.findViewById<Button>(R.id.select_cancel_bt)?.setOnClickListener {
+//
+//        }
+
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == 100){
+
+        }
+    }
+
 }
