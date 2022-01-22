f4b84cd3e8f0cf908d97aea35a4686ef80c4993f
lee989898 <wnsrud1352@gmail.com>
2022-01-22 11:36:44 +0900

    feat: join nickname 구현
    join nickname 닉네임 입력시 버튼 활성화
    다음 화면으로 이동

--- app/src/main/java/com/example/cornfarmer_android/JoinNicknameActivity.kt
+++ app/src/main/java/com/example/cornfarmer_android/JoinNicknameActivity.kt
@@ -1,13 +1,50 @@
 package com.example.cornfarmer_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import com.example.cornfarmer_android.databinding.ActivityJoinNicknameBinding
+import com.example.cornfarmer_android.databinding.ActivityJoinProfileBinding
 
 class JoinNicknameActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityJoinNicknameBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join_nickname)
+        binding = ActivityJoinNicknameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nicknameFinishColorIv.setOnClickListener {
+            val intent = Intent(this, JoinOttActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.nicknameNicknameEt.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+                if(binding.nicknameNicknameEt.length() == 0){
+                    binding.nicknameFinishIv.visibility = View.VISIBLE
+                    binding.nicknameFinishColorIv.visibility = View.GONE
+                }else{
+                    binding.nicknameFinishIv.visibility = View.GONE
+                    binding.nicknameFinishColorIv.visibility = View.VISIBLE
+                }
+
+            }
+
+        })
+
     }
 }
