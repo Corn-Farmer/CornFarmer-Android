69bff0259339d4980eda19a23c99bf733a29d979
lee989898 <wnsrud1352@gmail.com>
2022-02-03 10:10:11 +0900

    feat: 카카오 로그인

---
+++ app/src/main/java/com/example/corn_farmer/AuthCodeHandlerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.corn_farmer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cornfarmer_android.R
+
+class AuthCodeHandlerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth_code_handler)
+    }
+}
