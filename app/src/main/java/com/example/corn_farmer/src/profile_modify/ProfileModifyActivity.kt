26dd52bdfddc1cb09f77611292d99807b87b5cb5
lee989898 <wnsrud1352@gmail.com>
2022-02-08 17:40:02 +0900

    feat: edittext

--- app/src/main/java/com/example/corn_farmer/src/profile_modify/ProfileModifyActivity.kt
+++ app/src/main/java/com/example/corn_farmer/src/profile_modify/ProfileModifyActivity.kt
@@ -2,6 +2,7 @@ package com.example.corn_farmer.src.profile_modify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import com.example.cornfarmer_android.R
 import com.example.cornfarmer_android.databinding.ActivityProfileModifyBinding
@@ -14,6 +15,27 @@ class ProfileModifyActivity : AppCompatActivity() {
 
         binding = ActivityProfileModifyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val sharedPreferences = getSharedPreferences("join", MODE_PRIVATE)
+        val nickname = sharedPreferences.getString("nickname", null)
+        var sex : String = ""
+        val birthday = sharedPreferences.getString("birthday", null)
+        val isMale = sharedPreferences.getString("isMale",null)
+        val isFemale = sharedPreferences.getString("isFemale",null)
+
+        if(isMale==null){
+            sex = "여자"
+            Log.d("female","여자")
+        }
+        else if(isFemale==null){
+            sex = "남자"
+            Log.d("male","남자")
+        }
+
+
+        binding.modifyNicknameInfoEt.hint = nickname
+        binding.modifyGenderInfoEt.hint = sex
+        binding.modifyBirthInfoEt.hint = birthday
 
         binding.modifyCancelIv.setOnClickListener {
             finish()
