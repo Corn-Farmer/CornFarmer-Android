247ef012bd2e491bb1373dbaebf260c5f53be421
jhje5595 <35906526+jhje5595@users.noreply.github.com>
2022-03-08 18:00:05 +0900

    faet : 카카오톡 공유하기

--- app/src/main/java/com/corn/corn_farmer/src/detail/DetailFragment.kt
+++ app/src/main/java/com/corn/corn_farmer/src/detail/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.corn.corn_farmer.src.detail
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
@@ -26,13 +27,18 @@ import com.corn.corn_farmer.src.search_result.SearchResultFragment
 import com.corn.corn_farmer.src.wishlist.WishlistActivity
 import com.corn.cornfarmer_android.R
 import com.corn.cornfarmer_android.databinding.FragmentDetailBinding
+import com.kakao.sdk.common.util.KakaoCustomTabsClient
+import com.kakao.sdk.link.LinkClient
+import com.kakao.sdk.link.WebSharerClient
+import com.kakao.sdk.template.model.*
 
 class DetailFragment(val movieIdx: Int, val keywordIdx: Int, val keyword: String): Fragment(), DetailFragmentView {
     lateinit var binding: FragmentDetailBinding
     var likeCount = 0
     var likeCount_comment = 0
     var moviePhoto = ""
-
+    var movieTitle = ""
+    var movieGenre = ""
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -51,6 +57,71 @@ class DetailFragment(val movieIdx: Int, val keywordIdx: Int, val keyword: String
 
         binding.detailHeartBtnIv.setOnClickListener {
             startActivity(Intent(requireContext(), WishlistActivity::class.java))
+        }
+
+        binding.detailMovieShareImg.setOnClickListener {
+            val defaultFeed = FeedTemplate(
+                content = Content(
+                    title = movieTitle,
+                    description = movieGenre,
+                    imageUrl = moviePhoto,
+                    link = Link(
+                        webUrl = "https://developers.kakao.com",
+                        mobileWebUrl = "https://developers.kakao.com"
+                    )
+                ),
+                social = Social(
+                    likeCount = likeCount,
+                ),
+                buttons = listOf(
+                    Button(
+                        "앱으로 보기",
+                        Link(
+//                            androidExecParams = mapOf("key1" to "value1", "key2" to "value2"),
+//                            iosExecParams = mapOf("key1" to "value1", "key2" to "value2")
+                        )
+                    )
+                )
+            )
+            // 피드 메시지 보내기
+
+// 카카오톡 설치여부 확인
+            if (LinkClient.instance.isKakaoLinkAvailable(requireContext())) {
+                // 카카오톡으로 카카오링크 공유 가능
+                LinkClient.instance.defaultTemplate(requireContext(),defaultFeed) { linkResult, error ->
+                    if (error != null) {
+                        Log.e("test", "카카오링크 보내기 실패", error)
+                    }
+                    else if (linkResult != null) {
+                        Log.d("test", "카카오링크 보내기 성공 ${linkResult.intent}")
+                        startActivity(linkResult.intent)
+
+                        // 카카오링크 보내기에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+                        Log.w("test", "Warning Msg: ${linkResult.warningMsg}")
+                        Log.w("test", "Argument Msg: ${linkResult.argumentMsg}")
+                    }
+                }
+            } else {
+                // 카카오톡 미설치: 웹 공유 사용 권장
+                // 웹 공유 예시 코드
+                val sharerUrl = WebSharerClient.instance.defaultTemplateUri(defaultFeed)
+
+                // CustomTabs으로 웹 브라우저 열기
+
+                // 1. CustomTabs으로 Chrome 브라우저 열기
+                try {
+                    KakaoCustomTabsClient.openWithDefault(requireContext(), sharerUrl)
+                } catch(e: UnsupportedOperationException) {
+                    // Chrome 브라우저가 없을 때 예외처리
+                }
+
+                // 2. CustomTabs으로 디바이스 기본 브라우저 열기
+                try {
+                    KakaoCustomTabsClient.open(requireContext(), sharerUrl)
+                } catch (e: ActivityNotFoundException) {
+                    // 인터넷 브라우저가 없을 때 예외처리
+                }
+            }
         }
 
         return binding.root
@@ -141,7 +212,8 @@ class DetailFragment(val movieIdx: Int, val keywordIdx: Int, val keyword: String
             binding.detailMovieStoryTv.text = movieInfo.synopsis
             binding.detailNumberOfLikeTv.text = "${movieInfo?.likeCnt}명이 찜했어요."
             likeCount = movieInfo?.likeCnt
-
+            movieGenre = "#"+movieInfo.movieGenreList?.joinToString(separator = " #") //공유하기에 쓰이는 변수
+            movieTitle = movieInfo!!.movieName
             setViewMore(binding.detailMovieStoryTv, binding.viewMore)
             Glide.with(this!!).load(movieInfo!!.moviePhotoList[0]).into(binding.detailMovieImageIv)
             moviePhoto = movieInfo!!.moviePhotoList[0]
