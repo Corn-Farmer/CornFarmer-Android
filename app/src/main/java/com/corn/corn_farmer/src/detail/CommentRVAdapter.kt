184dab7d617c536527b9f052c047be1ac96ef85d
lee989898 <wnsrud1352@gmail.com>
2022-03-09 14:57:01 +0900

    feat: 신고하기 다이얼로그 추가

--- app/src/main/java/com/corn/corn_farmer/src/detail/CommentRVAdapter.kt
+++ app/src/main/java/com/corn/corn_farmer/src/detail/CommentRVAdapter.kt
@@ -1,12 +1,21 @@
 package com.corn.corn_farmer.src.detail
 
+import android.content.Intent
+import android.provider.MediaStore
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.corn.corn_farmer.MainActivity
 import com.corn.corn_farmer.src.detail.model.getReviewList
+import com.corn.cornfarmer_android.R
 import com.corn.cornfarmer_android.databinding.ItemCommentBinding
 
 class CommentRVAdapter(private val reviewList : ArrayList<getReviewList>, val token : String) : RecyclerView.Adapter<CommentRVAdapter.ViewHolder>() {
@@ -30,6 +39,39 @@ class CommentRVAdapter(private val reviewList : ArrayList<getReviewList>, val to
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(reviewList[position], position)
          var likeCnt = reviewList[position].likeCnt
+
+        holder.binding.detailCommentDeclarationIv.setOnClickListener {
+
+//            val mDialogView =
+//                LayoutInflater.from(this).inflate(R.layout.image_select_custom_dialog, null)
+//            val mBuilder = AlertDialog.Builder(this, R.style.SelectAlertDialog)
+//                .setView(mDialogView)
+//
+//            val alertDialog = mBuilder.show()
+//            alertDialog.window?.setGravity(Gravity.BOTTOM)
+//
+//            alertDialog.findViewById<Button>(R.id.select_camera_bt)?.setOnClickListener {
+//                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                takePictureIntent.resolveActivity(packageManager)
+//                startActivityForResult(takePictureIntent, 100)
+//                alertDialog.dismiss()
+//            }
+//
+//            alertDialog.findViewById<Button>(R.id.select_album_bt)?.setOnClickListener {
+//                val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+//                startActivityForResult(gallery, 200)
+//                alertDialog.dismiss()
+//            }
+//
+//            alertDialog.findViewById<ImageView>(R.id.select_cancel_bt)?.setOnClickListener {
+//                alertDialog.dismiss()
+//            }
+
+//            val intent = Intent(holder.itemView?.context, MainActivity::class.java)
+//            ContextCompat.startActivity(holder.itemView.context, intent, null)
+
+        }
+
         holder.binding.detailComentHeartOnIv.setOnClickListener {
             commentLikeBtnClickListener.onHeartClick(reviewList[position], position, token)
             if (token != "") {
