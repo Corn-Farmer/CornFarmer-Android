5ca3d889b4b379e6881f559929ce786472d095fa
lee989898 <wnsrud1352@gmail.com>
2022-03-09 19:54:03 +0900

    faet: 신고 액티비티 추가

--- app/src/main/java/com/corn/corn_farmer/src/detail/CommentRVAdapter.kt
+++ app/src/main/java/com/corn/corn_farmer/src/detail/CommentRVAdapter.kt
@@ -18,57 +18,49 @@ import com.corn.corn_farmer.src.detail.model.getReviewList
 import com.corn.cornfarmer_android.R
 import com.corn.cornfarmer_android.databinding.ItemCommentBinding
 
-class CommentRVAdapter(private val reviewList : ArrayList<getReviewList>, val token : String) : RecyclerView.Adapter<CommentRVAdapter.ViewHolder>() {
+class CommentRVAdapter(private val reviewList: ArrayList<getReviewList>, val token: String) :
+    RecyclerView.Adapter<CommentRVAdapter.ViewHolder>() {
 
-    interface CommentLikeBtnClickListener{
-        fun onHeartClick(getReviewList: getReviewList, position: Int, token : String)
+    interface CommentLikeBtnClickListener {
+        fun onHeartClick(getReviewList: getReviewList, position: Int, token: String)
     }
 
-    private lateinit var commentLikeBtnClickListener : CommentLikeBtnClickListener
+    private lateinit var commentLikeBtnClickListener: CommentLikeBtnClickListener
 
-    fun setCommentLikeBtnClickListener(heartClickListener : CommentLikeBtnClickListener) {
+    fun setCommentLikeBtnClickListener(heartClickListener: CommentLikeBtnClickListener) {
         commentLikeBtnClickListener = heartClickListener
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding : ItemCommentBinding = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding: ItemCommentBinding =
+            ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(reviewList[position], position)
-         var likeCnt = reviewList[position].likeCnt
+        var likeCnt = reviewList[position].likeCnt
 
         holder.binding.detailCommentDeclarationIv.setOnClickListener {
 
-//            val mDialogView =
-//                LayoutInflater.from(this).inflate(R.layout.image_select_custom_dialog, null)
-//            val mBuilder = AlertDialog.Builder(this, R.style.SelectAlertDialog)
-//                .setView(mDialogView)
-//
-//            val alertDialog = mBuilder.show()
-//            alertDialog.window?.setGravity(Gravity.BOTTOM)
-//
-//            alertDialog.findViewById<Button>(R.id.select_camera_bt)?.setOnClickListener {
-//                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//                takePictureIntent.resolveActivity(packageManager)
-//                startActivityForResult(takePictureIntent, 100)
-//                alertDialog.dismiss()
-//            }
-//
-//            alertDialog.findViewById<Button>(R.id.select_album_bt)?.setOnClickListener {
-//                val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-//                startActivityForResult(gallery, 200)
-//                alertDialog.dismiss()
-//            }
-//
-//            alertDialog.findViewById<ImageView>(R.id.select_cancel_bt)?.setOnClickListener {
-//                alertDialog.dismiss()
-//            }
+            val mDialogView =
+                LayoutInflater.from(holder.itemView.context)
+                    .inflate(R.layout.detail_declation_dialog, null)
+            val mBuilder = AlertDialog.Builder(holder.itemView.context, R.style.SelectDeclation)
+                .setView(mDialogView)
 
-//            val intent = Intent(holder.itemView?.context, MainActivity::class.java)
-//            ContextCompat.startActivity(holder.itemView.context, intent, null)
+            val alertDialog = mBuilder.show()
+//            alertDialog.window?.setGravity(Gravity.BOTTOM)
+
+            alertDialog.findViewById<ImageView>(R.id.detail_declation_button_iv)
+                ?.setOnClickListener {
+                    val intent = Intent(holder.itemView?.context, DeclationActivity::class.java)
+                    ContextCompat.startActivity(holder.itemView.context, intent, null)
+
+                    alertDialog.dismiss()
+                }
+
 
         }
 
@@ -93,17 +85,19 @@ class CommentRVAdapter(private val reviewList : ArrayList<getReviewList>, val to
     }
 
     override fun getItemCount(): Int {
-       return reviewList.size
+        return reviewList.size
     }
 
-    inner class ViewHolder(val binding : ItemCommentBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(reviewList : getReviewList, position: Int) {
-            var createAt = reviewList.createAt.substring(0,10)
+    inner class ViewHolder(val binding: ItemCommentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(reviewList: getReviewList, position: Int) {
+            var createAt = reviewList.createAt.substring(0, 10)
             binding.detailCommentUserNicknameTv.text = reviewList.writer.writerNickname
             binding.detailCommentTextTv.text = reviewList.contents
             binding.detailCommentDateTv.text = createAt
             binding.detailCommentLikecntTv.text = "${reviewList.likeCnt.toString()}"
-            Glide.with(itemView).load(reviewList.writer.writerPhoto).into(binding.detailCommentUserProfileIv)
+            Glide.with(itemView).load(reviewList.writer.writerPhoto)
+                .into(binding.detailCommentUserProfileIv)
             Log.d("writerPhoto", reviewList.writer.writerPhoto)
             if (reviewList.liked) {
                 binding.detailComentHeartOnIv.visibility = View.VISIBLE
---
+++ app/src/main/java/com/corn/corn_farmer/src/detail/DeclationActivity.kt
@@ -0,0 +1,12 @@
+package com.corn.corn_farmer.src.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.corn.cornfarmer_android.R
+
+class DeclationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_declation)
+    }
+}
