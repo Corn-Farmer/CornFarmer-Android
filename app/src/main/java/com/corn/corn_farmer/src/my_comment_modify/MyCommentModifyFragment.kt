76ef7c3ad309f0d23d1ae75612223ef96d499ba9
lee989898 <wnsrud1352@gmail.com>
2022-10-14 13:46:53 +0900

    [mod] #48 Toast.makeText -> showToast로 변경

--- app/src/main/java/com/corn/corn_farmer/src/my_comment_modify/MyCommentModifyFragment.kt
+++ app/src/main/java/com/corn/corn_farmer/src/my_comment_modify/MyCommentModifyFragment.kt
@@ -5,25 +5,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.corn.corn_farmer.MainActivity
 import com.corn.corn_farmer.src.my_comment.MyCommentFragment
 import com.corn.corn_farmer.src.my_comment_modify.model.getCommnetModifyAPI
 import com.corn.corn_farmer.src.my_comment_modify.model.sendModifyComment
+import com.corn.corn_farmer.util.ext.showToast
 import com.corn.cornfarmer_android.R
 import com.corn.cornfarmer_android.databinding.FragmentMyCommentModifyBinding
 
-class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx : Int, val nickname : String) : Fragment(), MyCommentModifyFragmentView {
-    private lateinit var binding : FragmentMyCommentModifyBinding
+class MyCommentModifyFragment(
+    val reviewInfo: sendModifyComment,
+    val reviewIdx: Int,
+    val nickname: String
+) : Fragment(), MyCommentModifyFragmentView {
+    private lateinit var binding: FragmentMyCommentModifyBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_my_comment_modify ,container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_my_comment_modify, container, false)
 
         binding.commentEdittext.setText(reviewInfo.content)
 
@@ -31,38 +36,37 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
         getRate()
 
         binding.commentCompleteBtnIv.setOnClickListener {
-            val sharedPreferences = this.activity?.getSharedPreferences("join", Context.MODE_PRIVATE)
+            val sharedPreferences =
+                this.activity?.getSharedPreferences("join", Context.MODE_PRIVATE)
             val servertoken = sharedPreferences?.getString("servertoken", null)
-            val newReview = sendModifyComment(binding.commentEdittext.text.toString(), binding.commentRateSaveTv.text.toString().toDouble())
+            val newReview = sendModifyComment(
+                binding.commentEdittext.text.toString(),
+                binding.commentRateSaveTv.text.toString().toDouble()
+            )
             val service = MyCommentModifyService(this, newReview, reviewIdx, servertoken!!)
             service.tryPutMyCommentModify()
         }
 
         binding.commentCancelBtnIv.setOnClickListener {
-            val mActivity = activity as MainActivity //메인 액티비티
+            val mActivity = activity as MainActivity // 메인 액티비티
             mActivity.callFragment(MyCommentFragment(nickname))
-
         }
 
         return binding.root
     }
 
     override fun onPutMyCommentModifySuccess(response: getCommnetModifyAPI) {
-
-        if(response.code == 2000){
-            Toast.makeText(context, response.message, Toast.LENGTH_SHORT).show()
-        }else{
-            val mActivity = activity as MainActivity //메인 액티비티
+        if (response.code == 2000) {
+            activity?.showToast(response.message)
+        } else {
+            val mActivity = activity as MainActivity // 메인 액티비티
             mActivity.callFragment(MyCommentFragment(nickname))
-            Toast.makeText(context, "후기를 수정하였습니다.", Toast.LENGTH_SHORT).show()
-
+            activity?.showToast("후기를 수정하였습니다.")
         }
-
-
     }
 
     override fun onPutMyCommentModifyFailure(message: String) {
-        Toast.makeText(context, "네트워크 연결에 실패했습니다.", Toast.LENGTH_SHORT).show()
+        activity?.showToast("네트워크 연결에 실패했습니다.")
     }
 
     fun setRate() {
@@ -90,7 +94,7 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn3Iv.visibility = View.VISIBLE
             binding.commentRateCorn4Iv.visibility = View.VISIBLE
             binding.commentRateCorn5Iv.visibility = View.VISIBLE
-        }else if (reviewInfo.rate <= 2.1) {
+        } else if (reviewInfo.rate <= 2.1) {
             binding.commentRateCorn1ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn2ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn3ColorIv.visibility = View.GONE
@@ -142,7 +146,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
     }
 
     fun getRate() {
-
         binding.commentRateSaveTv.text = reviewInfo.rate.toString()
 
         binding.commentRateCorn1Iv.setOnClickListener {
@@ -151,7 +154,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn3ColorIv.visibility = View.GONE
             binding.commentRateCorn4ColorIv.visibility = View.GONE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
-
 
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.VISIBLE
@@ -169,7 +171,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn4ColorIv.visibility = View.GONE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
 
-
             binding.commentRateCorn1Iv.visibility = View.VISIBLE
             binding.commentRateCorn2Iv.visibility = View.VISIBLE
             binding.commentRateCorn3Iv.visibility = View.VISIBLE
@@ -180,14 +181,11 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
         }
 
         binding.commentRateCorn2Iv.setOnClickListener {
-
-
             binding.commentRateCorn1ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn2ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn3ColorIv.visibility = View.GONE
             binding.commentRateCorn4ColorIv.visibility = View.GONE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
-
 
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.GONE
@@ -205,7 +203,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn4ColorIv.visibility = View.GONE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
 
-
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.VISIBLE
             binding.commentRateCorn3Iv.visibility = View.VISIBLE
@@ -221,7 +218,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn3ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn4ColorIv.visibility = View.GONE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
-
 
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.GONE
@@ -239,7 +235,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn4ColorIv.visibility = View.GONE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
 
-
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.GONE
             binding.commentRateCorn3Iv.visibility = View.VISIBLE
@@ -255,7 +250,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn3ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn4ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
-
 
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.GONE
@@ -273,7 +267,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn4ColorIv.visibility = View.GONE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
 
-
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.GONE
             binding.commentRateCorn3Iv.visibility = View.GONE
@@ -284,13 +277,11 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
         }
 
         binding.commentRateCorn5Iv.setOnClickListener {
-
             binding.commentRateCorn1ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn2ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn3ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn4ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn5ColorIv.visibility = View.VISIBLE
-
 
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.GONE
@@ -308,7 +299,6 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateCorn4ColorIv.visibility = View.VISIBLE
             binding.commentRateCorn5ColorIv.visibility = View.GONE
 
-
             binding.commentRateCorn1Iv.visibility = View.GONE
             binding.commentRateCorn2Iv.visibility = View.GONE
             binding.commentRateCorn3Iv.visibility = View.GONE
@@ -318,6 +308,4 @@ class MyCommentModifyFragment(val reviewInfo : sendModifyComment, val reviewIdx 
             binding.commentRateSaveTv.text = (4.0).toString()
         }
     }
-
-
 }
